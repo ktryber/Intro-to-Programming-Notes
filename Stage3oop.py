07c39fef3620e6cd5e62ad60f76924b4cd0f8120
Kris <Ktryber@gmail.com>
2015-05-10 14:49:56 -0400

    Stage 3 Submission 2

---
+++ Stage3oop.py
@@ -0,0 +1,14 @@
+
+
+class MyContent():
+	#This is a class that will be used to store a concept and it's description.
+	def __init__(self, title, description):
+		self.title = title
+		self.description = description
+		self.example = example
+
+
+
+
+
+
